b4fced93e28e140440564751070b99013452cbc0
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2024-03-14 20:45:43 +0100

    Add basic options

--- lua/options.lua
+++ lua/options.lua
@@ -1,3 +1,18 @@
 require "nvchad.options"
 
--- add yours here!
+vim.opt.relativenumber = true
+vim.opt.timeoutlen = 500 -- Delay whichkey a bit
+vim.g.lua_snippets_path = vim.fn.stdpath("config") .. "/lua/snippets"
+
+local autocmd = vim.api.nvim_create_autocmd
+
+-- Cleanup whitespace on save
+autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function()
+    local save_cursor = vim.fn.getpos(".")
+    -- pcall catches errors
+    pcall(function() vim.cmd [[%s/\s\+$//e]] end)
+    vim.fn.setpos(".", save_cursor)
+  end,
+})
