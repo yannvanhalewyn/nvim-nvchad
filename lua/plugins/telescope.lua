814caf16541b2fd928f3476745a28bf567fda4c9
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2024-03-14 22:24:38 +0100

    Enable Telescope UI select and ripgrep

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = {
+      extensions_list = { "themes", "terms","ui-select" },
+    },
+    dependencies = { "BurntSushi/ripgrep" }
+  }
+}
