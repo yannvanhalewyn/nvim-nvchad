b3d7bfadfb3fec8a64d3efd1493c57f21fe529fe
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2024-03-16 04:03:30 +0100

    CMP confirm on tab

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -6,6 +6,15 @@ return {
     opts = {
       mapping = {
         ["<C-q>"] = cmp.mapping.close(),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.confirm()
+          elseif require("luasnip").expand_or_jumpable() then
+            vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
+          else
+            fallback()
+          end
+        end, { "i", "s", }),
       },
     }
   },
