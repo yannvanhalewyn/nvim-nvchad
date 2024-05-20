6ee3a08464db79de67fb24311b9106cf9977db21
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2024-05-20 02:32:17 +0200

    Add oil.nvim

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -4,6 +4,36 @@ return {
     lazy = false,
   },
   {
+    'stevearc/oil.nvim',
+    lazy = false, -- So we can start up with nvim .
+    opts = {
+      default_file_explorer = true,
+      -- view_options = {
+      --   show_hidden = true,
+      -- },
+      keymaps = {
+        ["g?"] = "actions.show_help",
+        ["<CR>"] = "actions.select",
+        ["<C-s>"] = "actions.select_vsplit",
+        ["<C-h>"] = "actions.select_split",
+        ["<C-t>"] = "actions.select_tab",
+        ["<C-p>"] = "actions.preview",
+        ["<C-c>"] = "actions.close",
+        ["<C-r>"] = "actions.refresh", -- Changed this for window switching as it was <C-l>
+        ["-"] = "actions.parent",
+        ["_"] = "actions.open_cwd",
+        ["`"] = "actions.cd",
+        ["~"] = "actions.tcd",
+        ["gs"] = "actions.change_sort",
+        ["gx"] = "actions.open_external",
+        ["g."] = "actions.toggle_hidden",
+        ["g\\"] = "actions.toggle_trash",
+      },
+    },
+    -- Optional dependencies
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+  },
+  {
     "karb94/neoscroll.nvim",
     enabled = not vim.g.neovide,
     -- ~2ms, not worth lazy loading
