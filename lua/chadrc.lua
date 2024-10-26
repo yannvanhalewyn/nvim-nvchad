d66657e07100ecd274e6ac23ca8da7a878598537
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2024-10-26 06:25:53 +0200

    Refactor diff colors after upgrading

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -78,28 +78,31 @@ M.base46 = {
     ["@comment"] = { italic = true },
     ["@symbol"] = { fg = "blue" },
     ["@function"] = { fg = "yellow" },
+    -- Used heavily by vim diff
     DiffAdd = { fg = "NONE", bg = "#31352b" },
     DiffDelete = { fg = "NONE", bg = "#511c21" },
     -- Part of changed line that actually changed
-    DiffText = { fg = "NONE", bg = "#373b43", bold = true },
+    -- DiffText = { fg = "NONE", bg = "#373b43", bold = true },
   },
 
   hl_add = {
     ["@function.call.clojure"] = { fg = "yellow" },
+    ["function.call"] = { fg = "yellow" },
     ["function.call.lua"] = { fg = "yellow" },
     -- ["@function.call"] = { fg = "yellow" },
     ["@string.special.symbol"] = { fg = "blue" },
 
-    NeogitDiffAdd = { fg = "#7d9c53" },
-    NeogitDiffAddHighlight = { link = "DiffAdd" },
-    -- Neogit uses get_fg("Error"), which is dark in my theme not red.
+    -- Deleted line in git status when not higlighted
     NeogitDiffDelete = { fg = "#e06c75" },
+    -- Deleted line in git status when higlighted
     NeogitDiffDeleteHighlight = { link = "DiffDelete" },
+    -- Shows 'midified' in yellow in git status
     NeogitChangeModified = { fg = "yellow" },
+    -- Shows 'deleted' in red in git status
     NeogitChangeDeleted = { fg = "red" },
-
-    DiffviewDiffChange = { fg = "NONE", bg = "#2d3139" },
-    DiffviewDiffAddAsDelete = { link = "DiffDelete" },
+    --
+    -- DiffviewDiffChange = { fg = "NONE", bg = "#2d3139" },
+    -- DiffviewDiffAddAsDelete = { link = "DiffDelete" },
   },
 }
 
